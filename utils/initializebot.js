09fe1db0bd71475f75ff41f40246446603f8105b
Moh <mohthegoat1@proton.me>
2025-09-15 11:26:56 +0100

    fix first launch crash (accessing db before init)

--- utils/initializebot.js
+++ utils/initializebot.js
@@ -6,10 +6,10 @@ const chalk = require("chalk");
 const {setAiIds} = require("./setaiids");
 module.exports = {
     async init( client, configpath) {
+        await initDb();
         const channels = await getaichannels()
         setAiIds(channels.map(item => item.ai_channel_id))
         await fixConfig();
-        await initDb();
         const config = JSON.parse(fs.readFileSync(path.resolve(configpath)));
         if (!config.ownerID) {
             console.warn(chalk.bgYellow.black('[WARN] config.ownerID is not defined! Owner-only commands will block all users.'));
