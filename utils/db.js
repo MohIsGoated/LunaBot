ac8ea8f451a94088eb5fe88f5a0461dcd92dc290
mohh <mohthegoat1@proton.me>
2025-10-09 14:55:00 +0100

    fix issue where the bot would crash if the user is already in the db in a different server for invit tracking

--- utils/db.js
+++ utils/db.js
@@ -9,22 +9,22 @@ if (!fs.existsSync(dataDir)) {
 }
 
 
-const db = new sqlite3.Database(dbpath)
+const db = new sqlite3.Database(dbpath);
         async function execute(db, sql, params = []) {
                 if (params.length > 0) {
                         return new Promise((resolve, reject) => {
                                 db.run(sql, params, (err) => {
                                         if (err) reject(err)
                                         resolve()
-                                })
-                        })
+                                });
+                        });
                 }
                 return new Promise((resolve, reject) => {
                         db.exec(sql, (err) => {
                                 if (err) reject(err)
                                 resolve()
-                            })
-                })
+                            });
+                });
         }
 
 
@@ -33,8 +33,8 @@ const db = new sqlite3.Database(dbpath)
                         db.get(sql, params, (err, row) => {
                                 if (err) reject(err)
                                 resolve(row)
-                        })
-                })
+                        });
+                });
         }
 
 
